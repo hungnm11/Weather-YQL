0108ad280b6054c89cf14210ada6f0c7d011a4b6
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-06-27 11:27:47 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,52 +1,144 @@
 $(function() {
 
-    $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2502265&format=json&diagnostics=true&callback=', function(data) {
+    $.getJSON("js/myjson.json", function(data) {
         console.log(data);
-        var output = "";
-        output += "Location: " + data.query.results.channel.location.city + ", "+ data.query.results.channel.location.country + "<br/>";
-        output += data.query.results.channel.item.condition.date +"<br/>";
-        output += '<img src="http://l.yimg.com/a/i/us/we/52/' + data.query.results.channel.item.condition.code + '.gif" width="34" height="34" title="' + data.query.results.channel.item.condition.code + '" /><br/>';
-        output += data.query.results.channel.item.condition.temp+' &deg;F<br/>';
-        output += data.query.results.channel.item.condition.text;
+        var speed = 500;
+        var autoSwitch = true;
+        var autoSwitch_speed = 4000;
         
-        var winddirection=parseInt(data.query.results.channel.wind.direction);
-        var direction='';
-        switch(true)
+        var output = "";
+        var outputNumber = "";
+        var countObj = data.query.results.channel.length;
+        
+        console.log(countObj);
+        
+        for (var i = 0; i < countObj; i++)
         {
-            case (winddirection==0):
-                direction='N';
-                break;
-            case (winddirection<90):
-                direction='NE';
-                break;
-            case (winddirection==90):
-                direction='E';
-                break;
-            case (winddirection<180):
-                direction='SE';
-                break;
-            case (winddirection==180):
-                direction='S';
-                break;
-            case (winddirection<270):
-                direction='SW';
-                break;
-            case (winddirection==270):
-                direction='W';
-                break;
-            case (winddirection<360):
-                direction='NW';
-                break;
-            case (winddirection==360):
-                direction='N';
-                break;
+            output += "<div class='slide'>";
+            output += "Location: " + data.query.results.channel[i].location.city + ", "+ data.query.results.channel[i].location.country + "<br/>";
+            output += data.query.results.channel[i].item.condition.date +"<br/>";
+            output += '<img src="http://l.yimg.com/a/i/us/we/52/' + data.query.results.channel[i].item.condition.code + '.gif" width="34" height="34" title="' + data.query.results.channel[i].item.condition.code + '" /><br/>';
+            output += data.query.results.channel[i].item.condition.temp+' &deg;F<br/>';
+            output += data.query.results.channel[i].item.condition.text;
+
+            var winddirection=parseInt(data.query.results.channel[i].wind.direction);
+            var direction='';
+            switch(true)
+            {
+                case (winddirection==0):
+                    direction='N';
+                    break;
+                case (winddirection<90):
+                    direction='NE';
+                    break;
+                case (winddirection==90):
+                    direction='E';
+                    break;
+                case (winddirection<180):
+                    direction='SE';
+                    break;
+                case (winddirection==180):
+                    direction='S';
+                    break;
+                case (winddirection<270):
+                    direction='SW';
+                    break;
+                case (winddirection==270):
+                    direction='W';
+                    break;
+                case (winddirection<360):
+                    direction='NW';
+                    break;
+                case (winddirection==360):
+                    direction='N';
+                    break;
+            }
+
+            output += 'Wind: '+direction+' at '+data.query.results.channel[i].wind.speed+' mph<br/>';
+            output += 'Humidity: '+data.query.results.channel[i].atmosphere.humidity+'%<br/>'
+            output += 'Check ' + "<a href='"+data.query.results.channel[i].item.link+"'>Full Forecast</a><br/><br/>";
+            output += "</div>";
+            
+            outputNumber += "<div class='numberSlide'>" + (i+1) + "</div>";
+            
+            document.getElementById("slider").innerHTML = output;
+            
+            document.getElementById("countSlide").innerHTML = outputNumber;
         }
         
-        output += 'Wind: '+direction+' at '+data.query.results.channel.wind.speed+' mph<br/>';
-        output += 'Humidity: '+data.query.results.channel.atmosphere.humidity+'%<br/>'
-        output += 'Check ' + "<a href='"+data.query.results.channel.item.link+"'>Full Forecast</a>";
         
-        document.getElementById("result").innerHTML = output;
+
+        //Add active to the first slide
+        $('.slide').first().addClass('active');
+        $('.numberSlide').first().addClass('active');
+
+        // Hide all slide
+        $('.slide').hide();
+
+        // Show the first slide
+        $('.active').show();
+
+        $('#next').on('click', function(){
+            nextSlide();
+        });
+
+        $('#prev').on('click', function() {
+		prevSlide();
+        });
+
+        $('.numberSlide').on('click', function() {
+            slideNumber($(this));
+        });
+
+        function prevSlide() {
+            $('.active').removeClass('active').addClass('oldActive');
+            if($('.oldActive').is(':first-child')) {
+                $('.slide').last().addClass('active');
+                $('.numberSlide').last().addClass('active');
+            } else {
+                $('.oldActive').prev().addClass('active');
+            }
+
+            $('.oldActive').removeClass('oldActive');
+            $('.slide').fadeOut(speed);
+            $('.active').fadeIn(speed);
+        }
+
+        function nextSlide() {
+            $('.active').removeClass('active').addClass('oldActive');
+            if($('.oldActive').is(':last-child')) {
+                $('.slide').first().addClass('active');
+                $('.numberSlide').first().addClass('active');
+            } else {
+                $('.oldActive').next().addClass('active');
+            }
+
+            $('.oldActive').removeClass('oldActive');
+            $('.slide').fadeOut(speed);
+            $('.active').fadeIn(speed);
+        }
+
+        function slideNumber(elem) {
+            //Count element
+            var count_elem = $('#slider').children().length;
+            var getOrinalElem = elem.text();
+
+            for (var i = 1; i <= count_elem; i++) {
+                if (getOrinalElem == i) {
+                    $('.active').removeClass('active');
+                    $('.slide:nth-child(' + getOrinalElem + ')').addClass('active');
+                    $('.slide').fadeOut(speed);
+                    $('.active').fadeIn(speed);
+                }
+            }
+
+            elem.addClass('active');
+        }
+
+
+        if(autoSwitch == true) {
+            setInterval(function() { nextSlide();}, autoSwitch_speed);
+        }
         
     });
     
